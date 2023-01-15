1dfc268743b0e3279b121b8fd6936eb2fc995690
nextgrid1 <victor_bailiot@hotmail.com>
2023-01-15 15:26:27 -0400

    Update v2

--- v2/assets/js/js.js
+++ v2/assets/js/js.js
@@ -26,7 +26,7 @@ class ColorPalette {
 
     setColors() {
         // pick a random hue somewhere between 220 and 360
-        this.hue = ~~random(220, 360);
+        this.hue = ~~random(120, 360);
         this.complimentaryHue1 = this.hue + 30;
         this.complimentaryHue2 = this.hue + 60;
         // define a fixed saturation and lightness
@@ -226,3 +226,52 @@ document
             orb.fill = colorPalette.randomColor();
         });
     });
+
+/* -------------------------------------------------------------- */
+
+const toggleButton = document.querySelector('.toggle'),
+    sidebar = document.querySelector('.sidebar'),
+    navItems = document.querySelectorAll('.nav-item')
+
+function changeToggleIcon() {
+    if (sidebar.classList.contains('open')) {
+        toggleButton.querySelector('span').textContent = 'chevron_left'
+    } else {
+        toggleButton.querySelector('span').textContent = 'chevron_right'
+    }
+}
+
+toggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('open')
+    changeToggleIcon()
+})
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener('click', () => {
+        navItems.forEach((navItem) => navItem.classList.remove('active'))
+        navItem.classList.add('active')
+    })
+})
+
+let touchstartX = 0
+let touchendX = 0
+
+function checkDirection() {
+    if (touchendX < touchstartX && sidebar.classList.contains('open') && touchendX - touchstartX < -30) {
+        sidebar.classList.remove('open')
+        changeToggleIcon()
+    }
+    if (touchendX > touchstartX && !sidebar.classList.contains('open') && touchendX - touchstartX > 30) {
+        sidebar.classList.add('open')
+        changeToggleIcon()
+    }
+}
+
+sidebar.addEventListener('touchstart', (e) => {
+    touchstartX = e.changedTouches[0].screenX
+})
+
+sidebar.addEventListener('touchend', (e) => {
+    touchendX = e.changedTouches[0].screenX
+    checkDirection()
+})
